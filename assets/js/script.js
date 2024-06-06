a40228dde51830ee77d536f7b1310e3978eee100
Maris Feagin <mfeagin64@gmail.com>
2024-06-06 10:49:18 -0400

    a bit of css and js to load map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lgn: 150.644},
-      zoom: 8,
-      mapId: '67f4e362b2cfd7dd',
-    });
+
+  const centerMap = {lat: 36.042787, lng: -161.6333136,}
+   
+   const mapOptions = {
+     center: centerMap,
+     zoom: 8,
+   } 
+  
+   const map = new google.maps.Map(document.getElementById('map'), mapOptions);
 };
