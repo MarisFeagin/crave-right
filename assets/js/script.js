02cfbbecd6d17d0933d9c597f75d05f0703406bf
Maris Feagin <mfeagin64@gmail.com>
2024-06-05 22:15:28 -0400

    added map

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-var placeholder = document.querySelector
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lgn: 150.644},
+      zoom: 8,
+      mapId: '67f4e362b2cfd7dd',
+    });
+};
