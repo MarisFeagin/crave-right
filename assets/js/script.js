c64405ab67421ab658af7991641b89b8913a3282
Maris Feagin <mfeagin64@gmail.com>
2024-06-07 18:50:29 -0400

    gen css and map init

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,41 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// Map JS
+var map = L.map('map').setView([39.38064969597025, -97.90948071443827], 5);
 
       var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       })
     
       osm.addTo(map);
+
+// Drop down JS
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+
+// Slider JS
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+};
+
+
