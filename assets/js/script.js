9e493bfcaae068ec91bd62a381b8db14f45dd4c9
Maris Feagin <mfeagin64@gmail.com>
2024-06-11 17:38:13 -0400

    added business type selection to form

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,27 +40,27 @@ var map = L.map('map').setView([39.38064969597025, -97.90948071443827], 5);
 
 // Filter Markers
 let myJSON=[
-  {
-    lat:10,
-    lon:10,
-    demographic:['youth']
-  },
-  {
-    lat:6,
-    lon:10,
-    demographic:['adults','men']
-  },
-  {
-    lat:10,
-    lon:12,
-    demographic:['adults']
-  },
-  {
-    lat:7,
-    lon:8,
-    demographic:['adults','seniors','women']
-  }
-]
+     {
+       lat:10,
+       lon:10,
+       demographic:['youth']
+     },
+     {
+       lat:6,
+       lon:10,
+       demographic:['adults','men']
+     },
+     {
+       lat:10,
+       lon:12,
+       demographic:['adults']
+     },
+     {
+       lat:7,
+       lon:8,
+       demographic:['adults','seniors','women']
+     }
+   ]
 
 let myMarkers = L.featureGroup().addTo(map);
 
@@ -69,39 +69,39 @@ btnForm.onclick=()=>filter();
 
 function filter() {
 
-myMarkers.clearLayers();
+	myMarkers.clearLayers();
 
-let myJSONcopy = JSON.parse(JSON.stringify(myJSON));
+	let myJSONcopy = JSON.parse(JSON.stringify(myJSON));
 
-let categories=[];
-
-if (cbAdults.checked) {
- categories.push('adults');
-}
-if (cbYouth.checked) {
- categories.push('youth');
-}
-if (cbSeniors.checked) {
- categories.push('seniors')
-}
-if (cbMen.checked) {
- categories.push('men');
-}
-if (cbWomen.checked) {
- categories.push('women');
-}
-
-
-for (let i=0;i<myJSONcopy.length;i++){
-for (let j=0;j<categories.length;j++){
-  for (let k=0;k<myJSONcopy[i].demographic.length;k++)
-   if (categories[j]==myJSONcopy[i].demographic[k] && !myJSONcopy[i].added){
-     L.circleMarker([myJSONcopy[i].lat,myJSONcopy[i].lon]).addTo(myMarkers);
-     myJSONcopy[i].added=true;
-     break;
+	let categories=[];
+  
+	if (cbAdults.checked) {
+  	categories.push('adults');
+  }
+  if (cbYouth.checked) {
+  	categories.push('youth');
+  }
+  if (cbSeniors.checked) {
+  	categories.push('seniors')
+  }
+  if (cbMen.checked) {
+  	categories.push('men');
+  }
+  if (cbWomen.checked) {
+  	categories.push('women');
+  }
+  
+  
+  for (let i=0;i<myJSONcopy.length;i++){
+   for (let j=0;j<categories.length;j++){
+   	for (let k=0;k<myJSONcopy[i].demographic.length;k++)
+    	if (categories[j]==myJSONcopy[i].demographic[k] && !myJSONcopy[i].added){
+      	L.circleMarker([myJSONcopy[i].lat,myJSONcopy[i].lon]).addTo(myMarkers);
+        myJSONcopy[i].added=true;
+        break;
+      }
    }
-}
-}
+  }
 }
 
 /* Dropdown Filter Menu */
