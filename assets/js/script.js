824235cf6282be5a30ddeb97c390854b5a65549f
Maris Feagin <mfeagin64@gmail.com>
2024-06-10 23:06:38 -0400

    restructured form

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,14 +28,12 @@ window.onclick = function(event) {
   }
 }
 
-// Slider JS Calories
-var sliderCal = document.getElementById("calories-per-meal");
-var outputCal = document.getElementsById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+// Slider Calories JS
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
 
-// Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
-};
-
+}
 
