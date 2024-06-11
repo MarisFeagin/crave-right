50ab4fd9ac4c754983ae9254dd930a7e542740e2
Maris Feagin <mfeagin64@gmail.com>
2024-06-11 16:43:21 -0400

    live user location

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,37 @@ var map = L.map('map').setView([39.38064969597025, -97.90948071443827], 5);
       })
     
       osm.addTo(map);
+  navigator.geolocation.watchPosition(success, error);
+
+  let marker, circle, zoomed;
+
+  function success(pos) {
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+    const accuracy = pos.coords.accuracy;
+
+    if (marker) {
+      map.removeLayer(marker);
+      map.removeLayer(circle);
+    }
+
+    marker = L.marker([lat, lng,]).addTo(map);
+    circle = L.circle([lat, lng,], { radius: accuracy }).addTo(map);
+
+    if (!zoomed) {
+      zoomed = map.fitBounds(circle.getBounds());
+    }
+
+    map.setView([lat, lng]);
+  }
+
+  function error(err) {
+    if (err.code === 1) {
+      alert("Please allow Crave Right to see your geolocation so we can match you with the best local food!");
+    } else {
+      alert("Crave Right can't find your location");
+    }
+  }
 
 /* Dropdown Filter Menu */
 /* When the user clicks on the button,
