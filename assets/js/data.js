0d3e6e9b25038c2051c091ffd5023bb8c22944c8
Maris Feagin <mfeagin64@gmail.com>
2024-10-25 19:19:37 -0400

    inital api mockup

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,49 +1,44 @@
 const express = require('express')();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
-const PORT = 8080;
+const PORT = 3000;
 
-app.use( express.json() )
+app.use( cors() );
+app.use( bodyParser.json() );
 
-app.listen(
-    PORT,
-    () => console.log(`It's alive on https://localhost:${PORT}`)
-)
+let nutritionLabels = [
 
-app.post('/menu-item/:id', (req, res) => {
+];
+
+// 'Disclaimer': `Crave Right does not claim to recommend diets nor nutrition advice to any of our users. We only arm you with the knowledge of a food's value and numbers. Please meet with a lincesed dietian and/or nutritionist for your individual comsumption needs. Everyone will require a different plan. Please consume responsibly!`
+
+// Get Labels
+app.get('/api/labels', (req, res) => {
+    res.json(nutritionLabels);
+});
+
+// Add new label
+app.post('/api/labels', (req, res) => {
+    const newLabel = { id: Date.now(), ...req.body };
+    nutritionLabels.push(newLabel);
+    res.status(201).json(newLabel);
+});
+
+// Update existing label
+app.put('/api/label/:id', (req, res) => {
     const { id } = req.params;
-    const { logo } = req.body;
-
-    if (!logo) {
-        res.status(418).send({ message: 'Nutrition Information has not been sourced at this time.'})
+    const index = nutritionLabels.findIndex(label => label.id == id);
+    if (index !== -1) {
+        nutritionLabels[index] = { id: parseInt(id), ...req.body };
+        res.json(nutritionLabels[index]);
+    } else {
+        res.status(404).json({ message: 'Label not found' });
     }
+});
 
-    res.send({
-        id: `item with your ${logo} and ID of ${id}`,
-
-        'title': `${name}`,
-        'Nutrition': `${servingnumber} per ${type}
-           Serving Size: ${servsize}
-           Total Size: ${totalsize}
-           [Insert THICC line here css or something]
-           Amount per serving
-           Calories: ${calories}
-           [Insert line here css or something]
-           (Listed in grams and onces)
-           Total Fat: ${fat}
-             Saturated Fat: ${satfat}
-             Trans Fat: ${transfat}
-           Cholesterol: ${cholest}
-           Sodium: ${sodium}
-           Total Carbohydrate: ${carbs}
-             Dietary Fiber: ${fiber}
-             Total Sugars: ${sugar}
-               Included Added Sugars: ${addsugar}
-           Protein: ${protein}
-           [Insert THICC line here css or something]
-           Vitamin D: ${vitD}
-           Calcium: ${calcium}
-           Iron: ${iron}
-           Potassium: ${potass}`,
-        'Disclaimer': `Crave Right does not claim to recommend diets nor nutrition advice to any of our users. We only arm you with the knowledge of a food's value and numbers. Please meet with a lincesed dietian and/or nutritionist for your individual comsumption needs. Everyone will require a different plan. Please consume responsibly!`
-    });
+// Start Server 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
