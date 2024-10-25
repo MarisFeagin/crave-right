6ff51807e43fe8aeecc801f78af273338e18ea0e
Maris Feagin <mfeagin64@gmail.com>
2024-10-24 20:42:01 -0400

    inital api call and response

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,8 +1,23 @@
-const app = require('express', '.../data.js')();
-
+const express = require('express')();
+const app = express();
 const PORT = 8080;
+
+app.use( express.json() )
 
 app.listen(
     PORT,
     () => console.log(`It's alive on https://localhost:${PORT}`)
 )
+
+app.post('/menu-item/:id', (req, res) => {
+    const { id } = req.params;
+    const { logo } = req.body;
+
+    if (!logo) {
+        res.status(418).send({ message: 'Nutrition Information has not been sourced at this time.'})
+    }
+
+    res.send({
+        id: `item with your ${logo} and ID of ${id}`,
+    });
+});
